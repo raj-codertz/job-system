190b921cf0035731250209ab364815be230cb676
suinegraj <rajabujshabani@gmail.com>
2023-09-08 08:58:29 +0300

    added useState in dashboardlayout page

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,7 +1,24 @@
 import Wrapper from "../assets/wrappers/Dashboard"
 import {BigSidebar, Navbar, SmallSidebar} from "../components"
 import { Outlet } from "react-router-dom"
+import { useState } from "react"
 const DashboardLayout = () => {
+
+    // temporary user
+    const user = { name: 'Rajab Shabani'}
+
+    const [showSidebar, setShowSidebar] = useState(false)
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+    const toggleDarkTheme = () => {
+        console.log('toggle dark theme')
+    }
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar)
+    }
+    const logoutUser = async () => {
+        console.log('logout user')
+    }
     return (
         <Wrapper>
             <main className='dashboard'>
