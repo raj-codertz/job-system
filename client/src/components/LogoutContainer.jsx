f91d6f28c098e85eb2800a6d804cc900a71e834f
suinegraj <rajabujshabani@gmail.com>
2023-09-09 17:46:23 +0300

    craeted a logoutcontainer component

---
+++ client/src/components/LogoutContainer.jsx
@@ -0,0 +1,13 @@
+import { FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import Wrapper from '../assets/wrappers/LogoutContainer';
+import { useState } from 'react';
+import { useDashboardContext } from '../pages/DashboardLayout';
+
+const LogoutContainer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default LogoutContainer
