793841102da288b4fc4edb7c02329716c9045e74
suinegraj <rajabujshabani@gmail.com>
2023-12-26 19:43:42 +0300

    updatesd a profile page

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -9,14 +9,6 @@ import {
     deleteJob} from '../controllers/jobController.js'
 import {validateIdParam, validationJobInput} from "../middleware/validationMiddleware.js";
 
-<<<<<<< HEAD
-// you can use normal method
-
-// router.get('/', getAllJobs)
-// router.post('/', createJob)
-
-=======
->>>>>>> b6380c0 (hidden dot env)
 // or you can use method chaining the controllers
 router.route('/').get(getAllJobs).post(validationJobInput, createJob)
 router.route('/:id').get(validateIdParam, getJob).patch(validationJobInput, validateIdParam, editJob).delete(validateIdParam, deleteJob)
