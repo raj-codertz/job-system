340f31b4dac1108f934dd1538d3b6cd9191e5d12
suinegraj <rajabujshabani@gmail.com>
2024-01-01 18:50:25 +0300

    added cloudinary setup

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import mongoose from "mongoose"
 import cookieParser from 'cookie-parser'
+import cloudinary from "cloudinary"
 
 // routers
 import jobRouter from './routes/jobRouter.js'
@@ -20,6 +21,14 @@ import path from 'path'
 // middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js"
 import {authenticateUser} from "./middleware/authMiddleware.js"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
