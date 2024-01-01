e396e0d143969bf304150d1ba3e84ea0280ae3ba
suinegraj <rajabujshabani@gmail.com>
2024-01-01 19:09:37 +0300

    added cloudinary setup  in profile.jsx

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,11 +14,23 @@ export const getApplicationStats = async (req, res) => {
     res.status(StatusCodes.OK).json({ user, job })
 }
 export const updateUser = async (req, res) => {
-    console.log(req.file);
-
     // removing password from req.body just in case if its in there since it doesn't use it for update
-    const obj = { ...req.body }
-    delete obj.password
-    const updatedUser = await User.findByIdAndUpdate( req.user.userId, obj)
+    const newUser = { ...req.body }
+    delete newUser.password
+
+    // checking if there is an avatar file from a user
+    if (req.file) {
+        const response = await cloudinary.v2.uploader.upload(req.file.path)
+        await fs.unlink(req.file.path)
+        newUser.avatar = response.secure_url
+        newUser.avatarPublicId = response.public_id
+    }
+    // this updateUser return an old instances of user's details then
+    const updatedUser = await User.findByIdAndUpdate( req.user.userId, newUser)
+
+    // checking if an old file avatar exist and delete from cloudinary
+    if (req.file && updatedUser.avatarPublicId) {
+        await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId)
+    }
     res.status(StatusCodes.OK).json({ msg: 'update user'})
 }
